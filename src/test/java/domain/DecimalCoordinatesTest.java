1fc06c3c0b732e478102465563c8f73c8fd950de
steve <steve.jones763@gmail.com>
2022-05-26 21:19:07 +0100

    Added further unit tests to domain objects with logic

---
+++ src/test/java/domain/DecimalCoordinatesTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+public class DecimalCoordinatesTest {
+
+    @Test
+    public void shouldReturnDoubleValueForLatitude() {
+        DecimalCoordinates underTest = new DecimalCoordinates(new BigDecimal("50.4896"), new BigDecimal("11.3868"));
+        assertThat(underTest.getLatitudeAsDouble(), is(50.4896));
+    }
+
+
+    @Test
+    public void shouldReturnDoubleValueForLongitude() {
+        DecimalCoordinates underTest = new DecimalCoordinates(new BigDecimal("50.4896"), new BigDecimal("11.3868"));
+        assertThat(underTest.getLongitudeAsDouble(), is(11.3868));
+    }
+
+}
---
+++ src/test/java/domain/UserTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class UserTest {
+
+    @Test
+    public void shouldReturnCoordinatesAsDecimalCoordinates() {
+        User underTest = new User(1, "first", "last", "email", "ip", 10.1, 10.1);
+
+        DecimalCoordinates expected = new DecimalCoordinates(BigDecimal.valueOf(10.1), BigDecimal.valueOf(10.1));
+        assertThat(underTest.getDecimalCoordinates(), is(expected));
+    }
+
+}
